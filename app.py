b01ecf551883d7ae4dac9e5234455761b6d38101
karastoyanov on macbookPRO <karastoqnov.alexandar@gmail.com>
2024-02-01 21:57:48 +0200

    python tasks template

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+
+@app.route('/python_tasks')
+def python_tasks():
+    return render_template('python_tasks.html')
+
 if __name__ == '__main__':
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.run(debug=True, host = '0.0.0.0', port = '5000')
---
+++ static/script.js
@@ -0,0 +1,4 @@
+// Redirect to Python tasks
+function pythonTasks() {
+    window.location.href = "{{ url_for('python_tasks') }}";
+}
