e415ceddf2261bf26c10eae5d2a2ef40dad99074
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-03-09 23:17:33 +0200

    code optimization

--- app.py
+++ app.py
@@ -372,4 +372,4 @@ def submit_solution():
 
 if __name__ == '__main__':
     app.config["TEMPLATES_AUTO_RELOAD"] = True
-    app.run(debug=True, host = '0.0.0.0', port = '5000')
+    app.run(debug=True, host = '0.0.0.0', port = os.getenv('DEBUG_PORT'))
