cb1341d112aaea168e8e01e8283494ba63528542
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-03-09 22:26:01 +0200

    code optimization

--- app.py
+++ app.py
@@ -370,8 +370,6 @@ def submit_solution():
     return user_output
 
 
-
-
 if __name__ == '__main__':
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     app.run(debug=True, host = '0.0.0.0', port = '5000')
