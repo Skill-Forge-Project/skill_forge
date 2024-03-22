5a1b4f0d3de1d82a432f0436dbdeabe9253f31ca
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-03-22 16:35:54 +0200

    user submited quest form fully implemented

--- user_submit_quest.py
+++ user_submit_quest.py
@@ -145,58 +145,87 @@ def open_submited_quest(quest_id):
 @login_required
 @app.route('/approve_submited_quest', methods=['GET', 'POST'])
 def approve_submited_quest():
-    # Read the values from the HTML form to pass to the Class constructor
-    submited_quest_id = request.form['submited_quest_id']
-    submited_quest_name = request.form['submited_quest_name']
-    submited_quest_language = request.form['submited_quest_language']
-    submited_quest_difficulty = request.form['submited_quest_difficulty']
-    submited_quest_author = request.form['submited_quest_author']
-    submited_quest_date_added=request.form['submited_quest_date_added']
-    submited_quest_condition = request.form['submited_quest_condition']
-    submited_function_template = request.form['submited_function_template']
-    submited_quest_unit_tests = request.form['submited_quest_unitests']
-    submited_quest_inputs = request.form['submited_quest_inputs']
-    submited_quest_outputs = request.form['submited_quest_outputs']
+    # Get the desired admin action
+    action = request.form['action']
     
-    print(f'Submited Quest Difficulty: {submited_quest_difficulty}')
-    # Assing XP points based on difficulty
-    xp = 0
-    type = ''
-    if request.form['submited_quest_difficulty'] == 'Novice Quests':
-        xp = 30
-        type = 'Basic'
-    elif request.form['submited_quest_difficulty'] == 'Adventurous Challenges':
-        xp = 60
-        type = 'Basic'
-    elif request.form['submited_quest_difficulty'] == 'Epic Campaigns':
-        xp = 100
-        type = 'Basic'
+    # IF the action is 'approve', then add the quest to the database class Quests
+        # The original submited quest should remain in the user_submited_quests table and change the status to 'Approved'
+    if action == "approve":
+        # Read the values from the HTML form to pass to the Class constructor
+        submited_quest_id = request.form['submited_quest_id']
+        submited_quest_name = request.form['submited_quest_name']
+        submited_quest_language = request.form['submited_quest_language']
+        submited_quest_difficulty = request.form['submited_quest_difficulty']
+        submited_quest_author = request.form['submited_quest_author']
+        submited_quest_date_added=request.form['submited_quest_date_added']
+        submited_quest_condition = request.form['submited_quest_condition']
+        submited_function_template = request.form['submited_function_template']
+        submited_quest_unit_tests = request.form['submited_quest_unitests']
+        submited_quest_inputs = request.form['submited_quest_inputs']
+        submited_quest_outputs = request.form['submited_quest_outputs']
+        
+        print(f'Submited Quest Difficulty: {submited_quest_difficulty}')
+        # Assing XP points based on difficulty
+        xp = 0
+        type = ''
+        if request.form['submited_quest_difficulty'] == 'Novice Quests':
+            xp = 30
+            type = 'Basic'
+        elif request.form['submited_quest_difficulty'] == 'Adventurous Challenges':
+            xp = 60
+            type = 'Basic'
+        elif request.form['submited_quest_difficulty'] == 'Epic Campaigns':
+            xp = 100
+            type = 'Basic'
+        
+        # Get the current time
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        
+        # Create a new Quest object
+        new_quest = Quest(
+            quest_id=submited_quest_id,
+            language=submited_quest_language,
+            difficulty=submited_quest_difficulty,
+            quest_name=submited_quest_name,
+            quest_author=submited_quest_author,
+            date_added=submited_quest_date_added,
+            last_modified=current_time,
+            condition=submited_quest_condition,
+            function_template=submited_function_template,
+            unit_tests=submited_quest_unit_tests,
+            test_inputs=submited_quest_inputs,
+            test_outputs=submited_quest_outputs,
+            xp=str(xp),
+            type=type
+        )
+        
+        # Update the quest status in user_submited_quests table
+        submited_quest = SubmitedQuest.query.filter_by(quest_id=submited_quest_id).first()
+        submited_quest.status = 'Approved'
+        
+        # Commit the changes to the database
+        db.session.add(new_quest)
+        db.session.commit()
+        
+        return redirect(url_for('open_admin_panel'))
     
-    # Get the current time
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # IF the action is 'reject', then change the status of the submited quest to 'Rejected'
+        # The original submited quest should remain in the user_submited_quests table. It can be modified in the future.
+    elif action == "reject":
+        quest_id = request.form['submited_quest_id']
+        submited_quest = SubmitedQuest.query.filter_by(quest_id=quest_id).first()
+        submited_quest.status = 'Rejected'
+        db.session.commit()
+        return redirect(url_for('open_admin_panel'))
     
-    new_quest = Quest(
-        quest_id=submited_quest_id,
-        language=submited_quest_language,
-        difficulty=submited_quest_difficulty,
-        quest_name=submited_quest_name,
-        quest_author=submited_quest_author,
-        date_added=submited_quest_date_added,
-        last_modified=current_time,
-        condition=submited_quest_condition,
-        function_template=submited_function_template,
-        unit_tests=submited_quest_unit_tests,
-        test_inputs=submited_quest_inputs,
-        test_outputs=submited_quest_outputs,
-        xp=str(xp),
-        type=type
-    )
-    db.session.add(new_quest)
-    # db.session.delete(submited_quest) Instead of delete, change the status to 'Approved' and disable the buttons for Approve/Request Changes
-    db.session.commit()
-    print("Quest was submitted successfully!")
-    return redirect(url_for('open_admin_panel'))
-
+    # IF the action is 'request-changes', then change the status of the submited quest to 'Pending'
+        # The original submited quest should remain in the user_submited_quests table. It can be modified in the future.
+    elif action == 'request-changes':
+        quest_id = request.form['submited_quest_id']
+        submited_quest = SubmitedQuest.query.filter_by(quest_id=quest_id).first()
+        submited_quest.status = 'Pending'
+        db.session.commit()
+        return redirect(url_for('open_admin_panel'))
 
 # Post new comment in comments sections
 @login_required
