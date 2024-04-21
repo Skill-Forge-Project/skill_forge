9df9c5b0943bfa9bbb265c44518f57ae44bb8a36
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-04-21 15:29:52 +0300

    main page info updated

--- static/js/markdownSupport.js
+++ static/js/markdownSupport.js
@@ -18,3 +18,26 @@ textarea.addEventListener("input", updateHtmlOutput);
 
 // Initial update
 updateHtmlOutput();
+
+
+
+// Get the textarea element
+var textareaTwo = document.getElementById("markdown-content");
+// Get the div element for HTML output
+var htmlOutputTwo = document.getElementById("html-content-output");
+
+// Initialize Showdown converter
+var converterTwo = new showdown.Converter();
+
+// Convert Markdown text to HTML and display in the HTML output div
+function updateHtmlOutputTwo() {
+    var markdownTextTwo = textareaTwo.value;
+    var htmlTextTwo = converter.makeHtml(markdownTextTwo);
+    htmlOutputTwo.innerHTML = htmlTextTwo;
+}
+
+// Update HTML output when the textarea content changes
+textareaTwo.addEventListener("input", updateHtmlOutputTwo);
+
+// Initial update
+updateHtmlOutputTwo();
