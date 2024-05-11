74f6cdba5fcdb8ffedc5079d114d814c06ae4ae3
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-05-11 20:00:04 +0300

    class:User migrations

--- app.py
+++ app.py
@@ -80,6 +80,9 @@ class User(UserMixin, db.Model):
     github_profile = db.Column(db.String(120), default=" ")
     discord_id = db.Column(db.String(120), default=" ")
     linked_in = db.Column(db.String(120), default=" ")
+    is_banned = db.Column(db.Boolean, default=False)
+    ban_date = db.Column(db.DateTime, default=None)
+    ban_reason = db.Column(db.String(120), default=" ")
 
     # Class constuctor
     def __init__(self, username, first_name, last_name, password, email):
