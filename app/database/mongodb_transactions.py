afb3d9e6563d2297c8cf7fc75b9a45df64500e5e
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-05-24 15:29:49 +0300

    mongodb transaction login/logout update

--- app/database/mongodb_transactions.py
+++ app/database/mongodb_transactions.py
@@ -4,12 +4,12 @@ from app.database.mongodb_init import mongo1_db, mongo1_client
 from app.database.mongodb_init import mongo2_db, mongo2_client
 
 # Create a transaction for inserting a log entry in the userLogins or userLoggouts collection
-def session_transaction(collection, user_id, username, login_time):
+def session_transaction(collection, user_id, username, timestamp):
     user_logins_collection = mongo1_db[f'{collection}']
     session = mongo1_client.start_session()
     with session.start_transaction():
         try:
-            user_logins_collection.insert_one({'user_id': user_id, 'username': username, 'login_time': login_time}, session=session)
+            user_logins_collection.insert_one({'user_id': user_id, 'username': username, 'timestamp': timestamp}, session=session)
         except Exception as e:
             print(f'Error while inserting log entry in userLogin collection for user: {username}, Error: {e}')
             session.abort_transaction()
