86eb3c458913420aedc33be9b9730af91d1d7075
karastoyanov on Nexus-Dev <karastoqnov.alexandar@gmail.com>
2024-06-06 00:43:03 +0300

    python code runner update

--- app/code_runners/run_python.py
+++ app/code_runners/run_python.py
@@ -1,5 +1,4 @@
-import subprocess
-import re
+import subprocess, os, re, uuid
 
 def run_code(python_code, inputs, outputs):
     tests_count = len(inputs)
@@ -7,46 +6,54 @@ def run_code(python_code, inputs, outputs):
     unsuccessful_tests = 0
     zero_tests = [] # Hold the first example test input and putput
     zero_tests_outputs = [] # Hold the first example after executing the user code (stdout & stderr)
-
+    execution_id = str(uuid.uuid4())
+    workdir = f"/tmp/{execution_id}"
+    os.makedirs(workdir, exist_ok=True)
+    
+    # Create new Python file in /tmp directory
+    code_filename = f'user_code_{execution_id}.py'
+    # Get the function name
+    function_name = re.findall(r"(?<=def ).*(?=\()", python_code)
+    
     for i in range(tests_count):
         current_input = [f'"{element}"' if isinstance(element, str) else str(element) for element in inputs[i]]
         current_input = ', '.join(current_input)
         correct_output = str(outputs[i][0])
-
-        function_name = re.findall(r"(?<=def ).*(?=\()", python_code)
-        if current_input.isalpha():
-            current_python_code = python_code + '\n\n' + f'print({function_name[0]}("{current_input}"))'
-        else:
-            current_python_code = python_code + '\n\n' + f'print({function_name[0]}({current_input}))'
-
-        # Use subprocess to run Python code
-        process = subprocess.Popen(['python', '-c', current_python_code], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
         
-        # Decode output from bytes to string
-        stdout_str = stdout.decode('utf-8').replace('\n', '')
-        stderr_str = stderr.decode('utf-8')
+        # Concatenate the Python code with the function call
+        current_execute = python_code + '\n\n' + f'print({function_name[0]}({current_input}))'
+        # Write Python code to file
+        with open(os.path.join(workdir, code_filename), 'w') as f:
+            f.write(current_execute)
+
+        # Execute the Python code
+        run_command = ['firejail', '--quiet', '--noprofile', '--net=none', '--private', '--private-tmp', f'--whitelist={workdir}',
+                       'python3', os.path.join(workdir, code_filename)] + current_input.split()
+        run_process = subprocess.Popen(run_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = run_process.communicate()
         
-        # Handle the zero test case
+        stdout_str = stdout.decode('utf-8').strip()
+        stderr_str = stderr.decode('utf-8').strip()
+        
         if i == 0:
             zero_tests.append(current_input)
             zero_tests.append(correct_output)
             zero_tests_outputs.append(stdout_str)
             zero_tests_outputs.append(stderr_str)
-        
-        if correct_output == stdout_str:
+
+        if stdout_str == correct_output:
             successful_tests += 1
         else:
             unsuccessful_tests += 1
-        
-        if unsuccessful_tests == 0:
-            message = 'Congratulations! Your solution is correct!'
-        elif successful_tests > 0 and unsuccessful_tests > 0:
-            message = 'Your solution is partially correct! Try again!'
-        elif successful_tests == 0 and unsuccessful_tests > 0:
-            message = 'Your solution is incorrect! Try again!'
-    return successful_tests, unsuccessful_tests, message, zero_tests, zero_tests_outputs
 
+    if unsuccessful_tests == 0:
+        message = 'Congratulations! Your solution is correct!'
+    elif successful_tests > 0 and unsuccessful_tests > 0:
+        message = 'Your solution is partially correct! Try again!'
+    else:
+        message = 'Your solution is incorrect! Try again!'
+    
+    return successful_tests, unsuccessful_tests, message, zero_tests, zero_tests_outputs
 
 # Example Python code
 # python_code = """
