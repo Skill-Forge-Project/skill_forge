11745aae781c1763dc2ba991c982d4673d63ddbb
karastoyanov on SF-Host <karastoqnov.alexandar@gmail.com>
2024-09-28 00:05:19 +0300

    database setup update

--- app/database/mongodb_init.py
+++ app/database/mongodb_init.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from pymongo import MongoClient
 import os
+import urllib.parse
 
 load_dotenv()
 
@@ -12,12 +13,20 @@ MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
 MONGO_DATABASE = os.getenv('MONGO_DATABASE')
 MONGO_AUTH_SOURCE = os.getenv('MONGO_AUTH_SOURCE')
 MONGO_PRIMARY_PORT = int(os.getenv('MONGO_PRIMARY_PORT'))
+REPLICA_SET = os.getenv('REPLICA_SET')
 
 
 # Load the env variables
+# mongo1_client = MongoClient(
+#     host=os.getenv('MONGO_DATABASE_URI'))
 mongo1_client = MongoClient(
-    host=os.getenv('MONGO_DATABASE_URI_DEV'))
-
+    host=MONGO_HOSTNAME,
+    port=MONGO_PRIMARY_PORT,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    authSource=MONGO_AUTH_SOURCE,
+    replicaSet=REPLICA_SET
+)
 
 # Connect to the MongoDB with the provided URI(primary and secondary clients)
 mongo1_db = mongo1_client[MONGO_DATABASE]
